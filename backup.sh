37a0acc893804b7290fb1dcbc1ad394fa5229608
Rafael Pereira <83137321+RafaelPereira014@users.noreply.github.com>
2024-02-28 15:15:37 -0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@
 REMOTE_HOST="172.22.130.131"
 REMOTE_DIR="/root/backups/"
 LOCAL_DIR="/root/backups/"
-SSH_PASSWORD="R4ua#YVjJ8HNB3T!LxCS"
+SSH_PASSWORD="***********"
 
 # Copy each directory from the remote machine to the local machine
 if sshpass -p "$SSH_PASSWORD" scp -r $REMOTE_HOST:$REMOTE_DIR* $LOCAL_DIR; then
